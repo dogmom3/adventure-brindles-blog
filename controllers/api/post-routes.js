661990343a8c80643e96e4df334aa8fc5a49378e
dogmom3 <blair.seivers@yahoo.com>
2022-05-18 18:16:40 -0400

    add ability to comment

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -102,13 +102,14 @@ router.post("/", (req, res) => {
 
 // PUT /api/posts/upvote
 router.put("/upvote", (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Paws })
+ if(req.session){
+   Post.upvote({...req.body, user_id: req.session.user_id}, {Paws, Comment, User})
     .then((updatedPostData) => res.json(updatedPostData))
     .catch((err) => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
+  }
 });
 
 //updates existing post
