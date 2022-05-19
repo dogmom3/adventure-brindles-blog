547c98bcd4d60079eb93296a09204dfc5cb20909
dogmom3 <blair.seivers@yahoo.com>
2022-05-19 07:55:30 -0400

    trying to get post to display on comments page

---
+++ __tests__/helpers.test.js
@@ -0,0 +1,21 @@
+const {format_date} = require('../utils/helpers');
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2020-03-20 16:12:03');
+  
+    expect(format_date(date)).toBe('3/20/2020');
+  });
+
+  module.exports = {
+    format_date: date => {
+      // same logic as before...
+     
+    },
+    format_plural: (word, amount) => {
+      if (amount !== 1) {
+        return `${word}s`;
+      }
+  
+      return word;
+    }
+  }
