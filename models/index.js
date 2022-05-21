0790efea8879a6c98ab4909bfec0f9e3953e004d
dogmom3 <blair.seivers@yahoo.com>
2022-05-21 13:06:46 -0400

    debuggin fixed

--- models/index.js
+++ models/index.js
@@ -3,69 +3,71 @@ const Post = require('./Post');
 const Paws = require('./Paws');
 const Comment = require('./Comment');
 
-//create associations
-User.hasMany(Post, {
-    foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-
-});
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  foreignKey: 'user_id',
+onDelete: 'CASCADE'
 });
 
-//many-to-many association
-User.belongsToMany(Post, {
-    through: Paws,
-    as: 'pawed_posts',
-    foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+onDelete: 'CASCADE'
+});
 
-  });
-  Post.belongsToMany(User, {
-    through: Paws,
-    as: 'pawed_posts',
-    foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-  });
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+onDelete: 'CASCADE'
+});
 
-  //one-to-many user vote associations
-  Paws.belongsTo(User, {
-    foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-  });
-  Paws.belongsTo(Post, {
-    foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-  });
-  User.hasMany(Paws, {
-    foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+Post.hasMany(Paws, {
+  foreignKey: 'post_id',
+onDelete: 'CASCADE'
+});
 
-  });
-  Post.hasMany(Paws, {
-    foreignKey: 'post_id',
-  onDelete: 'SET NULL'
 
-  });
 
-  //comment associations
-  Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-  });
-  Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-  });
-  User.hasMany(Comment, {
-    foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-  });
-  Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+// //create associations
+// User.hasMany(Post, {
+//     foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
 
-  });
+// });
+
+
+// User.belongsToMany(Post, {
+//     through: Paws,
+//     as: 'pawed_posts',
+//     foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+
+//   });
+//   Post.belongsToMany(User, {
+//     through: Paws,
+//     as: 'pawed_posts',
+//     foreignKey: 'post_id',
+//   onDelete: 'CASCADE'
+//   });
+
+//   Paws.belongsTo(User, {
+//     foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+//   });
+//   Paws.belongsTo(Post, {
+//     foreignKey: 'post_id',
+//   onDelete: 'CASCADE'
+//   });
+//   User.hasMany(Paws, {
+//     foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+//   });
+ 
+//   Comment.belongsTo(Post, {
+//     foreignKey: 'post_id',
+//   onDelete: 'CASCADE'
+//   });
+//   User.hasMany(Comment, {
+//     foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+//   });
+  
 
 module.exports = { User, Post, Paws, Comment };
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,27 +4,32 @@ const postdata = [
   {
     title: 'Post 1',
     post_url: 'https://instagram.com/adventure.brindles',
-    user_id: 1
+    user_id: 1,
+    content: 'content'
   },
   {
     title: 'Post 2',
     post_url: 'https://instagram.com/adventure.brindles',
-    user_id: 2
+    user_id: 2,
+    content: 'content'
   },
   {
     title: 'Post 3',
     post_url: 'https://instagram.com/adventure.brindles',
-    user_id: 3
+    user_id: 3,
+    content: 'content'
   },
   {
     title: 'Post 4',
     post_url: 'https://instagram.com/adventure.brindles',
-    user_id: 4
+    user_id: 4,
+    content: 'content'
   },
   {
     title: 'Post 5',
     post_url: 'https://instagram.com/adventure.brindles',
-    user_id: 5
+    user_id: 5,
+    content: 'content'
   }
 ];
 
