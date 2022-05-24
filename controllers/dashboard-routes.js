e412653a6fbbfa43b4801aa7940d8a9793e8e63f
dogmom3 <blair.seivers@yahoo.com>
2022-05-24 00:39:21 -0400

    need to get post page display diff than edit page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Paw } = require('../models');
+const { Post, User, Comment,Paw } = require('../models');
 const withAuth = require('../utils/auth');
 
 //GET ALL POSTS
