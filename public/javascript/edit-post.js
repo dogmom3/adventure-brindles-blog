447da6a7ebfe92902ded54ca7eff8aa40b929672
dogmom3 <blair.seivers@yahoo.com>
2022-05-25 13:37:35 -0400

    edit post page not working

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,6 +2,7 @@ async function editFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value.trim();
+  // const content = document.querySelector('input[name="content"]').value;
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
@@ -12,10 +13,13 @@ async function editFormHandler(event) {
       method: 'PUT',
       body: JSON.stringify({
         title, content
+
       }),
+
       headers: {
         'Content-Type': 'application/json'
       }
+      
     });
   
     if (response.ok) {
@@ -26,4 +30,5 @@ async function editFormHandler(event) {
   }
 }
   document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  // document.querySelector('.save-post-btn').addEventListener('submit', editFormHandler);
   
